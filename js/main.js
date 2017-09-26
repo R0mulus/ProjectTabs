cf71924291311d9443ba99315d9fbcfa13b9a9ad
R0mulus <jozef.balint@akademiasovy.sk>
2017-09-26 07:43:19 +0200

    Added bootstrap and working result table

--- js/main.js
+++ js/main.js
@@ -3,36 +3,81 @@ $(document).ready(function(){
   var btnFormConfirm = $('#btnFormConfirm');
   var inputFirstName = $('#inputFirstName');
   var inputLastName = $('#inputLastName');
+  var inputGender;
+  var inputDOB = $('#inputDOB');
+  var resultTable = $('#tableResults');
+  var resultTableContainer = $('#divTableResults');
+  var sequence = 0;
+
+  //warning messages variables
+  var warningFirstName = $('#warningMsgFirstName');
+  var warningLastName = $('#warningMsgLastName');
+  var warningDOB = $('#warningMsgDOB');
+  var warningGender = $('#warningMsgGender');
+  var correctInput = [0,0,0,0];
+
 
   btnFormConfirm.on('click', function(){
-    console.log('btn OK clicked');
+
 
     var firstNameLength = $.trim(inputFirstName.val());
     var lastNameLength = $.trim(inputLastName.val());
-
+    var dobLenght = $.trim(inputDOB.val());
 
     if(firstNameLength.length === 0){
-      console.log("Enter first name!");
+      warningFirstName.text('Enter first name!');
+      correctInput[0] = 0;
     }else{
-      console.log(inputFirstName.val() + "\n" + firstNameLength.length);
+      warningFirstName.text('');
+      correctInput[0] = 1;
     }
 
     if(lastNameLength.length === 0){
-      console.log("Enter last name!");
+      warningLastName.text('Enter last name!');
+      correctInput[1] = 0;
     }else{
-      console.log(inputLastName.val() + "\n" + lastNameLength.length);
+      warningLastName.text('');
+      correctInput[1] = 1;
     }
 
 
     if ($("input[name='gender']:checked").length === 0) {
-      console.log("Choose gender!");
+      warningGender.text('Choose gender!');
+      correctInput[2] = 0;
     }else{
-      var inputGender = $("input[name='gender']:checked").val();
-      console.log(inputGender);
+      inputGender = $("input[name='gender']:checked").val();
+      warningGender.text('');
+      correctInput[2] = 1;
     }
 
+    if(dobLenght.length === 0){
+      warningDOB.text('Choose date!');
+      correctInput[3] = 0;
+    }else{
+      warningDOB.text('');
+      correctInput[3] = 1;
+    }
+
+    var sum = 0;
+    for(var i = 0; i < correctInput.length; i++){
+      sum += correctInput[i];
+    }
+    if(sum === 4){
+      resultTableContainer.removeClass("hide");
+      resultTable.append("<tr id='person" + sequence + "><td>" + inputFirstName.val() + "</td>" +
+        "<td>" + inputLastName.val() + "</td>" +
+        "<td>" + $.trim(inputDOB.val()) + "</td>" +
+        '<td class="icon"><span class="glyphicon glyphicon-trash remove-button"></span></td>' +
+        "</tr>");
+    }
+
+    $('.remove-button').on('click', function(){
+      $(this).parent().parent().remove();
+
+    });
 
   });
+
 
 
 
